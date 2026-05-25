01af62ef6be3ead802ef327ecea9c8b4a5e526ff
Kjanoudii <janoudikhaled@gmail.com>
2026-05-25 09:50:35 +0300

    google anala

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 // import "./globals.css";
 import "./tailwind.css"
 import Navbar from "@/components/NavBar";
@@ -18,7 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-EGMREM91QB" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EGMREM91QB');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
       <Navbar />
         
